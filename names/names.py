653050b7ed441a0fea2f7c349e5e38c46bea61f0
siratl <elishubax@gmail.com>
2019-05-17 09:36:53 -0700

    Optimized runtime in names.py

--- names/names.py
+++ names/names.py
@@ -10,11 +10,25 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+names_1.sort()
+names_2.sort()
+
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+a = 0
+b = 0
+while a < len(names_1) and b < len(names_2):
+    if names_1[a] == names_2[b]:
+        duplicates.append(names_1[a])
+        a += 1
+        b += 1
+    elif names_1[a] < names_2[b]:
+        a += 1
+    else:
+        b += 1
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
